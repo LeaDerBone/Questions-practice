16718a1f159f82c5941a0b35f7fe22a17980a31d
Lil Hoon <47732431+LeaDerBone@users.noreply.github.com>
2022-12-22 21:21:41 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import './App.css';
+import data from './data';
+import SingleQuestion from './Question';
+
+function App() {
+  const [questions, setQuestions] = useState(data);
+  return (
+    <div className='container'>
+        <h3>Questions</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question}></SingleQuestion>
+            );
+          })}
+        </section>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const questions = [
+  {
+    id: 1,
+    title: 'Messi better than Ronaldo?',
+    info:
+      "Of course, He's Goat. He won 2022 World Cup. Game Over right? Ronaldo is finished.",
+  },
+  {
+    id: 2,
+    title: 'Ishowspeed is the best youtuber ever?',
+    info:
+      "Just 1 year he got 12M Followers. That's why He's Goat.",
+  },
+  {
+    id: 3,
+    title: 'Yamato is the best character?',
+    info:
+      'Just like messi and speed, She Or He is Goat.',
+  },
+  {
+    id: 4,
+    title: 'One Piece is the best anime?',
+    info:
+      'Everybody knows it. Best Anime and manga in the world!!',
+  },
+  {
+    id: 5,
+    title: 'Do you love god?',
+    info:
+      'Hohoho. No.',
+  }
+  ];
+  
+  export default questions;
